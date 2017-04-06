9ea56fe74cf03ce5c377e00ae3f36ae46736ebc2
hulei <hulei@nonobank.com>
2017-04-06 10:05:43 +0800

    KindEditorResult pojo

---
+++ taotao-common/src/main/java/com/taotao/commom/pojo/KindEditorResult.java
@@ -0,0 +1,52 @@
+package com.taotao.commom.pojo;
+
+public class KindEditorResult {
+	private int error;
+	
+	private String url;
+	
+	private String message;
+
+	public KindEditorResult(int error, String url, String message) {
+		super();
+		this.error = error;
+		this.url = url;
+		this.message = message;
+	}
+	
+	//成功
+	public static KindEditorResult ok(String url){
+		return new KindEditorResult(0, url, null);
+	}
+	
+	//失败
+	public static KindEditorResult error(){
+		return new KindEditorResult(1, null, "上传失败");
+	}
+	
+	public int getError() {
+		return error;
+	}
+
+	public void setError(int error) {
+		this.error = error;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
