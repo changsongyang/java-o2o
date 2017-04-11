573ef895dc1beba65f9d262da254738812ecb439
hulei <hulei@nonobank.com>
2017-04-11 16:53:48 +0800

    业务逻辑抽取。前端系统搭建

---
+++ taotao-rest/src/main/java/com/taotao/rest/controller/testController.java
@@ -0,0 +1,13 @@
+package com.taotao.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+	
+	@RequestMapping("/index")
+	public String test(){
+		return "index";
+	}
+}
