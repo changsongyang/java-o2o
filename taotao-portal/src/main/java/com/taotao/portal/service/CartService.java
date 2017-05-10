e490bd18da2ed625f8f1208c3073af7572ea699e
hulei <hulei@nonobank.com>
2017-05-10 12:40:05 +0800

    解决git 代码丢失

--- taotao-portal/src/main/java/com/taotao/portal/service/CartService.java
+++ taotao-portal/src/main/java/com/taotao/portal/service/CartService.java
@@ -1,10 +1,18 @@
 package com.taotao.portal.service;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.taotao.common.utils.TaotaoResult;
+import com.taotao.po.TbItem;
 
 public interface CartService {
+	
 	TaotaoResult cartItem(Long itemId,Integer itemNum,HttpServletRequest request,HttpServletResponse response);
+	List<TbItem> getTbItem(HttpServletRequest request);
+	TaotaoResult updateCart(HttpServletRequest request,HttpServletResponse response,Long itemid,Integer num);
+	TaotaoResult deleteCart(HttpServletRequest request,HttpServletResponse response,Long itemid);
+
 }
