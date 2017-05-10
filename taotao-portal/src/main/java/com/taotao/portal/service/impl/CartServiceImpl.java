008e8c2cb6ef4121007e617101f7d13a5e492838
hulei <hulei@nonobank.com>
2017-05-10 12:19:02 +0800

    Revert "购物车修改商品 删除商品"
    This reverts commit ef0f26e894fb1c6849d65042e1254329e9c07fc9.

--- taotao-portal/src/main/java/com/taotao/portal/service/impl/CartServiceImpl.java
+++ taotao-portal/src/main/java/com/taotao/portal/service/impl/CartServiceImpl.java
@@ -12,46 +12,44 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.utils.CookieUtils;
-import com.taotao.common.utils.ExceptionUtil;
 import com.taotao.common.utils.JsonUtils;
 import com.taotao.common.utils.TaotaoResult;
 import com.taotao.po.TbItem;
 import com.taotao.portal.service.CartService;
 import com.taotao.portal.service.ItemService;
-
 /**
- * 商品缓存信息 取商品缓存信息 更新商品缓存信息 删除商品缓存信息
- * 
+ * 商品缓存信息
  * @author hulei
  *
  */
 @Service
 public class CartServiceImpl implements CartService {
-
+	
 	@Autowired
 	ItemService itemService;
 	@Value("${COOKIE_EXPIRE}")
 	private Integer COOKIE_EXPIRE;
-
 	@Override
 	public TaotaoResult cartItem(Long itemId, Integer itemNum, HttpServletRequest request,
 			HttpServletResponse response) {
-		// 从cookie里取商品信息
+		//从cookie里取商品信息
 		List<TbItem> list = getTbItem(request);
 		Boolean flog = false;
 		for (TbItem tbItem : list) {
-			// 商品是否存在
-			if (tbItem.getId().longValue() == itemId) {
-				// 更新商品的数量
+			//商品已存在
+			if(tbItem.getId().longValue() == itemId){
+				System.out.println("存在");
+				//更新商品的数量
 				tbItem.setNum(tbItem.getNum() + itemNum);
 				flog = true;
 				break;
 			}
 		}
-		// 如果cookie中没有商品信息,查询商品信息存入cookie
-		if (!flog) {
+		//如果cookie中没有商品信息,查询商品信息存入cookie
+		if(!flog){
+			System.out.println("不存在");
 			TbItem result = itemService.getItemById(itemId);
-			if (!StringUtils.isBlank(result.getImage())) {
+			if(!StringUtils.isBlank(result.getImage())){
 				String imgs = result.getImage();
 				String[] img = imgs.split(",");
 				result.setImage(img[0]);
@@ -59,63 +57,24 @@ public class CartServiceImpl implements CartService {
 			result.setNum(itemNum);
 			list.add(result);
 		}
-		// 存入cookie
+		//存入cookie
 		CookieUtils.setCookie(request, response, "TT_CART", JsonUtils.objectToJson(list), COOKIE_EXPIRE, true);
 		return TaotaoResult.ok();
 	}
-
-	public List<TbItem> getTbItem(HttpServletRequest request) {
+	
+	private List<TbItem> getTbItem(HttpServletRequest request){
 		try {
-			// 从cookie里取商品信息
+			//从cookie里取商品信息
 			String json = CookieUtils.getCookieValue(request, "TT_CART", true);
-			if (StringUtils.isBlank(json)) {
+			if(StringUtils.isBlank(json)){
 				return new ArrayList<TbItem>();
 			}
-			// 将json转为java对象
+			//将json转为java对象
 			List<TbItem> list = JsonUtils.jsonToList(json, TbItem.class);
 			return list;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ArrayList<TbItem>();
-		}
-
-	}
-
-	@Override
-	public TaotaoResult updateCart(HttpServletRequest request, HttpServletResponse response, Long itemid, Integer num) {
-		// 从cookie里取商品信息
-		List<TbItem> list = getTbItem(request);
-		for (TbItem tbItem : list) {
-			// 商品是否存在
-			if (tbItem.getId().longValue() == itemid) {
-				// 更新商品的数量
-				tbItem.setNum(num);
-				break;
-			}
-		}
-		// 存入cookie
-		CookieUtils.setCookie(request, response, "TT_CART", JsonUtils.objectToJson(list), COOKIE_EXPIRE, true);
-		return TaotaoResult.ok();
-	}
-
-	@Override
-	public TaotaoResult deleteCart(HttpServletRequest request, HttpServletResponse response, Long itemid) {
-		try {
-			// 从cookie里取商品信息
-			List<TbItem> list = getTbItem(request);
-			for (TbItem tbItem : list) {
-				// 商品是否存在
-				if (tbItem.getId().longValue() == itemid) {
-					list.remove(tbItem);
-					break;
-				}
-			}
-			// 存入cookie
-			CookieUtils.setCookie(request, response, "TT_CART", JsonUtils.objectToJson(list), COOKIE_EXPIRE, true);
-			return TaotaoResult.ok();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
 		}
 		
 	}
