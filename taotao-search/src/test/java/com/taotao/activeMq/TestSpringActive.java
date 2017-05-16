9d0357d3aec545bfef0e550a3a63af4641524931
hulei <hulei@nonobank.com>
2017-05-16 19:35:31 +0800

    activemq spring整合

---
+++ taotao-search/src/test/java/com/taotao/activeMq/TestSpringActive.java
@@ -0,0 +1,15 @@
+package com.taotao.activeMq;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpringActive {
+	@Test
+	public void testSpringActiveMq() throws Exception {
+		//初始化spring容器
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-activemq.xml");
+		//等待
+		System.in.read();
+	}
+}
